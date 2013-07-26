7333a9555f334448b58560ef53c3482ce9e8d449
jeppe <jeppe@Jeppe>
2013-07-27 01:03:09 +0200

    Added TickKeeperUpdate to update a list of Runables at every TickKeeper tick

---
+++ src/net/vectorgaming/varenas/framework/TickKeeperUpdate.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.vectorgaming.varenas.framework;
+
+import java.util.ArrayList;
+import org.bukkit.plugin.Plugin;
+
+/**
+ *
+ * @author jeppe
+ */
+public class TickKeeperUpdate extends TickKeeper{
+    private ArrayList<Runnable> tasks = new ArrayList();
+    public TickKeeperUpdate(int ticksPerTick, boolean synchronizedTick, Plugin plugin) {
+        super(ticksPerTick, synchronizedTick, plugin);
+    }
+    
+    public boolean addRunnable(Runnable task){
+        return tasks.add(task);
+    }
+    public boolean removeRunnable(Runnable task){
+        return tasks.remove(task);
+    }
+    public ArrayList<Runnable> getTasks(){
+        return tasks;
+    }
+    
+    @Override
+    public void tick() {
+        for(Runnable task : getTasks()){
+            task.run();
+        }
+    }
+}
