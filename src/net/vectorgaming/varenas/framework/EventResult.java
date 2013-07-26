ed162f94dcb234648ebd23d1677c25454069c4b8
Kenny Williams <kenny@vectorgaming.net>
2013-07-26 16:59:45 -0700

    EventResult added to VArenas

---
+++ src/net/vectorgaming/varenas/framework/EventResult.java
@@ -0,0 +1,20 @@
+
+package net.vectorgaming.varenas.framework;
+
+/**
+ *
+ * @author Kenny
+ */
+public enum EventResult 
+{
+    GAME_RUNNING(0),
+    ONE_WINNER(1),
+    TEAM_WINNER(2);
+    
+    private final int eventResult;
+    
+    EventResult(int i)
+    {
+        eventResult = i;
+    }
+}
