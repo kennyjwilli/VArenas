e7760555bd3cd5fda8dab4a10bafee72d52bf685
Kenny Williams <kenny@vectorgaming.net>
2013-07-27 18:16:30 -0700

    Updated PlayerDeathListener to use new method in Arena

--- src/net/vectorgaming/varenas/listeners/PlayerDeathListener.java
+++ src/net/vectorgaming/varenas/listeners/PlayerDeathListener.java
@@ -18,15 +18,9 @@ public class PlayerDeathListener implements Listener
     @EventHandler
     public void onDeath(PlayerDeathEvent event)
     {
-        Player dead = event.getEntity();
-        Player killer = dead.getKiller();
-        if(!ArenaManager.isPlayerInArena(dead)) return;
-        
-        Arena arena = ArenaManager.getArenaFromPlayer(dead);
-        arena.getStats().recordKill(killer, dead);
-        if(arena.getStats().getTotalKills() == 10)
-        {
-            Bukkit.broadcastMessage("Match over. Cant tell you who won cuz no method for it yet");
-        }
+        if(!ArenaManager.isPlayerInArena(event.getEntity())) 
+            return;
+        Arena arena = ArenaManager.getArenaFromPlayer(event.getEntity());
+        arena.onDeath(event);
     }
 }
