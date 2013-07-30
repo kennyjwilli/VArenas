aa989a1699395ba2f4493891a5e9985300a717db
Jeppe Vennekilde <jeppe@jeppes.info>
2013-07-30 13:13:32 +0200

    Removed unused imports

--- src/net/vectorgaming/varenas/listeners/PlayerDeathListener.java
+++ src/net/vectorgaming/varenas/listeners/PlayerDeathListener.java
@@ -3,8 +3,6 @@ package net.vectorgaming.varenas.listeners;
 
 import net.vectorgaming.varenas.ArenaManager;
 import net.vectorgaming.varenas.framework.Arena;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -18,8 +16,9 @@ public class PlayerDeathListener implements Listener
     @EventHandler
     public void onDeath(PlayerDeathEvent event)
     {
-        if(!ArenaManager.isPlayerInArena(event.getEntity())) 
+        if(!ArenaManager.isPlayerInArena(event.getEntity())) {
             return;
+        }
         Arena arena = ArenaManager.getArenaFromPlayer(event.getEntity());
         arena.onDeath(event);
     }
