4bfb853f3cafb21084c209930022407697606bb8
Jeppe Vennekilde <jeppe@jeppes.info>
2013-07-30 13:26:20 +0200

    Added code for handling player and entity death events if they are on a ArenaTeam

--- src/net/vectorgaming/varenas/framework/PVPTeamArena.java
+++ src/net/vectorgaming/varenas/framework/PVPTeamArena.java
@@ -1,6 +1,11 @@
 package net.vectorgaming.varenas.framework;
 
 import org.bukkit.World;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public abstract class PVPTeamArena extends TeamArena{
 
@@ -11,4 +16,45 @@ public abstract class PVPTeamArena extends TeamArena{
     public PVPTeamArena(String name, String type, World world) {
         super(name, type, world);
     }
+
+    public abstract void onTeamPlayerDeath(EntityEvent event, ArenaTeam killedTeam, ArenaTeam killingTeam);
+    
+    @Override
+    public void sendEndMessage() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onDeath(PlayerDeathEvent event) {
+        ArenaTeam killedTeam = getTeamManager().getTeam(event.getEntity());
+        ArenaTeam killingTeam = getTeamManager().getTeam(event.getEntity().getKiller());
+        //No reason to call onTeamPlayerDeath if no one was on a team
+        if(killedTeam != null || killingTeam != null){
+            onTeamPlayerDeath(event,killedTeam,killingTeam);
+        }
+    }
+
+    @Override
+    public void onRespawn(PlayerRespawnEvent event) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    
+    /**
+     * Due to teams being able to have entities on their side, mobs also need to be
+     * accounted for with @onTeamPlayerDeath()
+     * @param event
+     */
+    public void onEntityDamage(EntityDamageEvent event){
+        if(event instanceof EntityDamageByEntityEvent) {
+            EntityDamageByEntityEvent eventDamage = (EntityDamageByEntityEvent) event;
+            ArenaTeam killedTeam = getTeamManager().getTeam(eventDamage.getEntity());
+            ArenaTeam killingTeam = getTeamManager().getTeam(eventDamage.getDamager());
+            //No reason to call onTeamPlayerDeath if no one was on a team
+            if(killedTeam != null || killingTeam != null){
+                onTeamPlayerDeath(event,killedTeam,killingTeam);
+            }
+        }
+    }
+    
 }
