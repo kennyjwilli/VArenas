1df7796b2b5bec1176ffd646b0035a9515a2894f
Kenny Williams <kenny@vectorgaming.net>
2013-08-04 20:10:13 -0700

    ArenaCreator added

---
+++ src/net/vectorgaming/varenas/ArenaCreator.java
@@ -0,0 +1,13 @@
+
+package net.vectorgaming.varenas;
+
+/**
+ *
+ * @author Kenny
+ */
+public abstract class ArenaCreator 
+{
+    public abstract void getNewArenaInstance();
+    
+    public abstract String getName();
+}
