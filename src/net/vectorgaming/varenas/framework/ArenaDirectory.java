fe053bb57d094ecdae0c01799a3959687e217953
Kenny Williams <kenny@vectorgaming.net>
2013-08-05 00:14:04 -0700

    Added ArenaDirectory enum to easily get the file path for commonly used directories

---
+++ src/net/vectorgaming/varenas/framework/ArenaDirectory.java
@@ -0,0 +1,40 @@
+
+package net.vectorgaming.varenas.framework;
+
+import java.io.File;
+import net.vectorgaming.varenas.ArenaAPI;
+
+/**
+ *
+ * @author Kenny
+ */
+public enum ArenaDirectory 
+{
+    /**
+     * Directory for all the framework files for each map
+     */
+    FRAMEWORK_DIR (ArenaAPI.getDataFolder().getAbsolutePath().toString()+File.separator+"framework"),
+    /**
+     * Directory for the arena settings file for each map
+     */
+    SETTINGS_DIR(ArenaAPI.getDataFolder().getAbsolutePath()+File.separator+"settings"),
+    /**
+     * Root directory of the server
+     */
+    SERVER_ROOT_DIR(""),
+    /**
+     * The location of the world for each map
+     */
+    MAPS_DIR (SERVER_ROOT_DIR+File.separator+"maps"),
+    /**
+     * The directory for all the running arenas
+     */
+    ARENAS_DIR (SERVER_ROOT_DIR+File.separator+"arenas");
+    
+    private final String file;
+    
+    ArenaDirectory(String f)
+    {
+        file = f;
+    }
+}
