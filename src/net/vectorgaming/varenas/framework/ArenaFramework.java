9e572a2542e9dd63edfa54aa6754111ffcffda34
Kenny Williams <kenny@vectorgaming.net>
2013-08-06 15:57:34 -0700

    Added content into arena framework so each arena's framework can be stored in the ram and saved on disable

--- src/net/vectorgaming/varenas/framework/ArenaFramework.java
+++ src/net/vectorgaming/varenas/framework/ArenaFramework.java
@@ -1,11 +1,75 @@
 
 package net.vectorgaming.varenas.framework;
 
+import info.jeppes.ZoneCore.TriggerBoxes.Point3D;
+import info.jeppes.ZoneCore.TriggerBoxes.TriggerBox;
+import java.util.HashMap;
+import org.bukkit.Location;
+
 /**
  *
  * @author Kenny
  */
 public class ArenaFramework 
 {
+    private HashMap<String, TriggerBox> triggerBoxMap = new HashMap<>();
+    private HashMap<String, Point3D> locationMap = new HashMap<>();
     
+    /**
+     * Adds a trigger box to the given path
+     * @param path Path to be saved in the YML file
+     * @param box TriggerBox
+     */
+    public void addTriggerBox(String path, TriggerBox box)
+    {
+        triggerBoxMap.put(path, box);
+    }
+    
+    /**
+     * Removes a trigger box 
+     * @param path Path saved in the YML file
+     */
+    public void removeTriggerBox(String path)
+    {
+        triggerBoxMap.remove(path);
+    }
+    
+    /**
+     * Gets a trigger box from the given path
+     * @param path Path saved in the YML file
+     * @return TriggerBox
+     */
+    public TriggerBox getTriggerBox(String path)
+    {
+        return triggerBoxMap.get(path);
+    }
+    
+    /**
+     * Adds a location to an arena
+     * @param path Path to be saved in the YML file
+     * @param loc Location 
+     */
+    public void addLocation(String path, Location loc)
+    {
+        locationMap.put(path, new Point3D(loc));
+    }
+    
+    /**
+     * Removed a location from the specified path
+     * @param path Path saved in the YML file
+     */
+    public void removeLocation(String path)
+    {
+        locationMap.remove(path);
+    }
+    
+    /**
+     * Gets the 3D point of a saved location
+     * @param path Path saved in the YML file
+     * @return Point3D
+     */
+    public Point3D getPoint3D(String path)
+    {
+        return locationMap.get(path);
+    }
 }
