e64b4a47ad225347a6f57ec5b7d26aea23623121
Kenny Williams <kenny@vectorgaming.net>
2013-08-06 20:34:19 -0700

    Moved to new package for more organization

---
+++ src/net/vectorgaming/varenas/framework/enums/ArenaType.java
@@ -0,0 +1,22 @@
+
+package net.vectorgaming.varenas.framework.enums;
+
+/**
+ *
+ * @author Kenny
+ */
+public enum ArenaType 
+{
+    MOB_ARENA("MOB_ARENA"),
+    PVP_ARENA("PVP_ARENA"),
+    SURVIVAL_GAMES("SURVIVAL_GAMES"),
+    TDM("TDM"),
+    CTF("CTF");
+    
+    private final String event;
+    
+    ArenaType(String e)
+    {
+        event = e;
+    }
+}
