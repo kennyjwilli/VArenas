f0165e2e4591caa1041513dce3620ee948a482e5
Kenny Williams <kenny@vectorgaming.net>
2013-08-09 01:42:33 -0700

    Updated file path for the server root directory.

--- src/net/vectorgaming/varenas/framework/enums/ArenaDirectory.java
+++ src/net/vectorgaming/varenas/framework/enums/ArenaDirectory.java
@@ -12,7 +12,7 @@ public class ArenaDirectory
 {
     public static final String ARENA_FRAMEWORK_DIR = ArenaAPI.getDataFolder().getAbsolutePath().toString()+File.separator+"framework";
     public static final String ARENA_SETTINGS_DIR = ArenaAPI.getDataFolder().getAbsolutePath().toString()+File.separator+"settings";
-    public static final String SERVER_ROOT_DIR = "";
+    public static final String SERVER_ROOT_DIR = new File("").getAbsolutePath();
     public static final String MAPS_DIR = SERVER_ROOT_DIR+File.separator+"maps";
     public static final String ARENAS_DIR = SERVER_ROOT_DIR+File.separator+"arenas";    
 //    /**
