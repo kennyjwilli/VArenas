6afa260648ecddb75475b31299538d55e52e8528
Kenny Williams <kenny@vectorgaming.net>
2013-08-09 00:12:51 -0700

    Moved package. Changed ArrayList to List

---
+++ src/net/vectorgaming/varenas/framework/interfaces/Settings.java
@@ -0,0 +1,46 @@
+
+package net.vectorgaming.varenas.framework.interfaces;
+
+import java.util.List;
+
+/**
+ *
+ * @author Kenny
+ */
+public interface Settings 
+{
+    public String getName();
+    
+    public void setBlockBreak(boolean value);
+    public boolean isBlockBreakEnabled();
+    
+    public void setTNTUse(boolean value);
+    public boolean isTNTEnabled();
+    
+    public void setMaxPlayers(int maxPlayers);
+    public Integer getMaxPlayers();
+    
+    public void setAuthors(String authors);
+    public String getAuthors();
+    
+    public void setObjective(String objecitve);
+    public String getObjective();
+    
+    public Integer getLobbyDuration();
+    public void setLobbyDuration(int duration);
+    
+    public List<String> getLobbyMessageInterval();
+    public void setLobbyMessageInterval(List<String> interval);
+    
+    public String getType();
+    public void setType(String type);
+    
+    public boolean isShowRespawnScreen();
+    public void setShowRespawnScreen(boolean value);
+    
+    public Integer getWinningKills();
+    public void setWinningKills(int winningKills);
+    
+    public Integer getGameDuration();
+    public void setGameDuration(int duration);
+}
