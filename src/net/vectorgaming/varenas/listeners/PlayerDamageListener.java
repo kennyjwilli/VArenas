0f70f662b4940c4a6ff0a61e15039eaa3012e191
Kenny Williams <kenny@vectorgaming.net>
2013-08-08 18:59:37 -0700

    Added PlayerDamageListener to listen for a player's death and cancel the damage if needed.

---
+++ src/net/vectorgaming/varenas/listeners/PlayerDamageListener.java
@@ -0,0 +1,48 @@
+
+package net.vectorgaming.varenas.listeners;
+
+import net.vectorgaming.varenas.ArenaManager;
+import net.vectorgaming.varenas.ArenaPlayerManager;
+import net.vectorgaming.varenas.framework.Arena;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+/**
+ *
+ * @author Kenny
+ */
+public class PlayerDamageListener implements Listener
+{
+    @EventHandler
+    public void onDamage(EntityDamageEvent event)
+    {
+        Player p;
+        if(event.getEntity() instanceof Player)
+        {
+            p = (Player) event.getEntity();
+        }else
+        {
+            return;
+        }
+        
+        if(!ArenaPlayerManager.isPlayerInArena(p))
+            return;
+        
+        String arenaName = ArenaPlayerManager.getArenaNameFromPlayer(p);
+        
+        if(!ArenaManager.isArenaRunning(arenaName))
+            return;
+        
+        Arena arena = ArenaManager.getArena(arenaName);
+        
+        if(event.getDamage() > p.getHealth())
+        {
+            ArenaPlayerManager.getArenaFromPlayer(p).onDeath(p, event.getEntity());
+            if(ArenaManager.getArenaSettings(arena).isShowRespawnScreen())
+                return;
+            event.setCancelled(true);
+        }
+    }
+}
