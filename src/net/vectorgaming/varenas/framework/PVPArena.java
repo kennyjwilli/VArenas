755d2df824b53ce130f1614e0a32cd4e669ce357
Kenny Williams <kenny@vectorgaming.net>
2013-08-08 23:32:50 -0700

    Game type now records kills and stops the match if the highest kills in the game are equal to the winning kills

--- src/net/vectorgaming/varenas/framework/PVPArena.java
+++ src/net/vectorgaming/varenas/framework/PVPArena.java
@@ -1,10 +1,13 @@
 
 package net.vectorgaming.varenas.framework;
 
+import info.jeppes.ZoneWorld.ZoneWorld;
+import net.vectorgaming.varenas.ArenaManager;
+import net.vectorgaming.varenas.ArenaPlayerManager;
 import net.vectorgaming.varenas.framework.stats.stats.KillCounter;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
@@ -14,18 +17,18 @@ import org.bukkit.event.player.PlayerRespawnEvent;
  */
 public class PVPArena extends TeamArena
 {
-    private boolean isRunning = false;
     private int TASK_ID;
     private int timeLeftLobby = 120;
     
     
-    public PVPArena(String name, String type, World world)
+    public PVPArena(String name, String type, ZoneWorld world)
     {
         super(name, type, world);
     }
     
     @Override
-    public void start(){
+    public void start()
+    {
         super.start();
         KillCounter killCounter = new KillCounter();
         getStats().addStat(killCounter);
@@ -35,7 +38,6 @@ public class PVPArena extends TeamArena
     public void forceStop() 
     {
         Bukkit.getScheduler().cancelTask(TASK_ID);
-        isRunning = false;
     }
 
     @Override
@@ -46,7 +48,8 @@ public class PVPArena extends TeamArena
     @Override
     public void onRespawn(PlayerRespawnEvent event)
     {
-        
+        Player p = event.getPlayer();
+        event.setRespawnLocation(this.getSpawnLocation(p));
     }
 
     @Override
@@ -56,7 +59,33 @@ public class PVPArena extends TeamArena
     }
 
     @Override
-    public void onDeath(PlayerDeathEvent event) {
+    public void onDeath(Player dead, Entity killer)
+    {
+        KillCounter kc = (KillCounter) this.getStats().getStat("killcounter");
+        
+        Player killerPlayer;
+        if(killer instanceof Player)
+        {
+            killerPlayer = (Player) killer;
+        }else
+        {
+            return;
+        }
+        
+        kc.recordKill(dead, killerPlayer);
+        
+        ArenaSettings settings = ArenaManager.getArenaSettings(ArenaPlayerManager.getArenaFromPlayer(dead));
+        Arena arena = ArenaPlayerManager.getArenaFromPlayer(dead);
+        
+        /*
+         * If winning kills is set to -1 then arena does not use kills to end the match. 
+         * Default is a timed match.
+         * Ends the arena if the highest kills is the same as the most kills.
+        */
+        if(settings.getWinningKills() <= 0 && kc.getHighestKills() >= settings.getWinningKills())
+        {
+            arena.end();
+        }
     }
 
 }
