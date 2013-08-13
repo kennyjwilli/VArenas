1ba0db9bb3826ae9e06ed19187d66572c87e0b7c
Kenny Williams <kenny@vectorgaming.net>
2013-08-13 11:56:49 -0700

    Added javadocs

--- src/net/vectorgaming/varenas/ArenaPlayerManager.java
+++ src/net/vectorgaming/varenas/ArenaPlayerManager.java
@@ -16,6 +16,11 @@ public class ArenaPlayerManager
     private static HashMap<Player, String> players = new HashMap<>();
     private static HashMap<String, ArrayList<Player>> arenas = new HashMap<>();
     
+    /**
+     * Adds a player to an arena
+     * @param arena Name of the arena
+     * @param p Bukkit player
+     */
     public static void addPlayerToArena(String arena, Player p) 
     {
         if(arenas.containsKey(arena))
@@ -33,26 +38,58 @@ public class ArenaPlayerManager
         players.put(p, arena);
     }
     
+    /**
+     * Adds a player to an arena based off a map type and an arena id
+     * @param map Map name that the arena is based off
+     * @param id Arena specific ID
+     * @param p Bukkit player
+     */
     public static void addPlayerToArena(String map, Integer id, Player p){addPlayerToArena(map+"_"+id, p);}
     
+    /**
+     * Removes a player from an arena
+     * @param arena Name of the arena
+     * @param p BNukkit player 
+     */
     public static void removePlayerFromArena(String arena, Player p) 
     {
         arenas.remove(arena);
         players.remove(p);
     }
     
+    /**
+     * Removes a player from an arena based off a map and an arena ID
+     * @param map Name of the map the arena is based off
+     * @param id Arena specific ID
+     * @param p Bukkit Player
+     */
     public static void removePlayerFromArena(String map, Integer id, Player p) {removePlayerFromArena(map+"_"+id, p);}
     
+    /**
+     * Gets the arena that the player is in
+     * @param p Bukkit player
+     * @return Arena object
+     */
     public static Arena getArenaFromPlayer(Player p)
     {
         return ArenaManager.getArena(players.get(p));
     }
     
+    /**
+     * Gets the arena name that the player is in 
+     * @param p Bukkit player
+     * @return Name of the arena
+     */
     public static String getArenaNameFromPlayer(Player p)
     {
         return players.get(p);
     }
     
+    /**
+     * Checks to see if the specified player is in the arena
+     * @param p Bukkit player
+     * @return boolean value
+     */
     public static boolean isPlayerInArena(Player p)
     {
         if(players.containsKey(p))
@@ -60,6 +97,11 @@ public class ArenaPlayerManager
         return false;
     }
     
+    /**
+     * Gets a list of all the players that are currently playing in the specified arena
+     * @param arena Name of the arena
+     * @return A list of Bukkit players
+     */
     public static ArrayList<Player> getPlayersInArena(String arena)
     {
         for(String s : arenas.keySet())
@@ -72,10 +114,23 @@ public class ArenaPlayerManager
         return null;
     }
     
+    /**
+     * Gets a list of all the players that are currently playing in the specified arena
+     * @param arena Arena object
+     * @return A list of players
+     */
     public static ArrayList<Player> getPlayersInArena(Arena arena) {return getPlayersInArena(arena.getName());}
     
+    /**
+     * Gets a list of all the players who are currently playing in an arena
+     * @return A list of Bukkit Players
+     */
     public static Set<Player> getAllArenaPlayers() {return players.keySet();}
     
+    /**
+     * Gets the total number of players who are currently in an arena
+     * @return The total number of arena players
+     */
     public static Integer getTotalArenaPlayers() {return players.keySet().size();}
     
 }
