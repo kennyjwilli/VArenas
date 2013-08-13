c7b47badad2d6c2f771db40877a646f470d739e5
Kenny Williams <kenny@vectorgaming.net>
2013-08-13 12:27:36 -0700

    Removed old code

--- src/net/vectorgaming/varenas/framework/enums/ArenaDirectory.java
+++ src/net/vectorgaming/varenas/framework/enums/ArenaDirectory.java
@@ -15,31 +15,4 @@ public class ArenaDirectory
     public static final String SERVER_ROOT_DIR = new File("").getAbsolutePath();
     public static final String MAPS_DIR = SERVER_ROOT_DIR+File.separator+"maps";
     public static final String ARENAS_DIR = SERVER_ROOT_DIR+File.separator+"arenas";    
-//    /**
-//     * Directory for all the framework files for each map
-//     */
-//    ARENA_FRAMEWORK_DIR (ArenaAPI.getDataFolder().getAbsolutePath().toString()+File.separator+"framework"),
-//    /**
-//     * Directory for the arena settings file for each map
-//     */
-//    ARENA_SETTINGS_DIR(ArenaAPI.getDataFolder().getAbsolutePath().toString()+File.separator+"settings"),
-//    /**
-//     * Root directory of the server
-//     */
-//    SERVER_ROOT_DIR(""),
-//    /**
-//     * The location of the world for each map
-//     */
-//    MAPS_DIR (SERVER_ROOT_DIR+File.separator+"maps"),
-//    /**
-//     * The directory for all the running arenas
-//     */
-//    ARENAS_DIR (SERVER_ROOT_DIR+File.separator+"arenas");
-//    
-//    private final String file;
-//    
-//    ArenaDirectory(String f)
-//    {
-//        file = f;
-//    }
 }
