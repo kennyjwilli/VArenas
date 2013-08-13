41451baa8debdf9cf04a60e187bfe28dee9e14bf
Kenny Williams <kenny@vectorgaming.net>
2013-08-13 11:56:23 -0700

    Added javadocs

--- src/net/vectorgaming/varenas/ArenaCreator.java
+++ src/net/vectorgaming/varenas/ArenaCreator.java
@@ -10,7 +10,18 @@ import net.vectorgaming.varenas.framework.Arena;
  */
 public abstract class ArenaCreator 
 {
+    /**
+     * Gets a new instance of an Arena object
+     * @param name Name of the arena
+     * @param map The map the arena is based off
+     * @param world The world the arena will be in
+     * @return Arena object
+     */
     public abstract Arena getNewArenaInstance(String name, String map, ZoneWorld world);
     
+    /**
+     * Gets the name of the arena
+     * @return Name of the arena
+     */
     public abstract String getName();
 }
