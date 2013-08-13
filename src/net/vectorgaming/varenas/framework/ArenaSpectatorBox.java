eb2b54967737ad8a9d8c4bb03365117c7b595e40
Kenny Williams <kenny@vectorgaming.net>
2013-08-13 12:24:00 -0700

    Updated javadocs and added new constructor

--- src/net/vectorgaming/varenas/framework/ArenaSpectatorBox.java
+++ src/net/vectorgaming/varenas/framework/ArenaSpectatorBox.java
@@ -13,10 +13,25 @@ import org.bukkit.Location;
  */
 public class ArenaSpectatorBox extends VRegion
 {
+    /**
+     *
+     * @param arena Name of the arena
+     * @param world World the arena is in
+     */
     public ArenaSpectatorBox(String arena, ZoneWorld world)
     {
         Arena a = ArenaManager.getArena(arena);
         Point3D point = a.getFramework().getSpectatorBoxSpawn();
         this.setSpawn(new Location(world.getCraftWorld(), point.x, point.y, point.z));
+    }
+    
+    /**
+     *
+     * @param arena Arena object
+     */
+    public ArenaSpectatorBox(Arena arena)
+    {
+        Point3D point = arena.getFramework().getSpectatorBoxSpawn();
+        this.setSpawn(new Location(arena.getWorld(), point.x, point.y, point.z));
     }
 }
