257b138b1490e80513fa9e51b3bedb8e3d59a082
Kenny Williams <kenny@vectorgaming.net>
2013-08-14 22:18:58 -0700

    Added directory for the kits. Removed "DIR" from all directory names

--- src/net/vectorgaming/varenas/framework/enums/ArenaDirectory.java
+++ src/net/vectorgaming/varenas/framework/enums/ArenaDirectory.java
@@ -10,9 +10,10 @@ import net.vectorgaming.varenas.ArenaAPI;
  */
 public class ArenaDirectory 
 {
-    public static final String ARENA_FRAMEWORK_DIR = ArenaAPI.getDataFolder().getAbsolutePath().toString()+File.separator+"framework";
-    public static final String ARENA_SETTINGS_DIR = ArenaAPI.getDataFolder().getAbsolutePath().toString()+File.separator+"settings";
-    public static final String SERVER_ROOT_DIR = new File("").getAbsolutePath();
-    public static final String MAPS_DIR = SERVER_ROOT_DIR+File.separator+"maps";
-    public static final String ARENAS_DIR = SERVER_ROOT_DIR+File.separator+"arenas";    
+    public static final String ARENA_FRAMEWORK = ArenaAPI.getDataFolder().getAbsolutePath().toString()+File.separator+"framework";
+    public static final String ARENA_SETTINGS = ArenaAPI.getDataFolder().getAbsolutePath().toString()+File.separator+"settings";
+    public static final String SERVER_ROOT = new File("").getAbsolutePath();
+    public static final String MAPS = SERVER_ROOT+File.separator+"maps";
+    public static final String ARENAS = SERVER_ROOT+File.separator+"arenas";
+    public static final String KITS = ArenaAPI.getDataFolder().getAbsolutePath()+File.separator+"kits";
 }
