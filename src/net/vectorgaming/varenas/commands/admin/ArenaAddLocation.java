b780c284b50af1aa5e293362d883b391ce8a1a9e
Kenny Williams <kenny@vectorgaming.net>
2013-08-14 22:20:17 -0700

    Removed unused import

--- src/net/vectorgaming/varenas/commands/admin/ArenaAddLocation.java
+++ src/net/vectorgaming/varenas/commands/admin/ArenaAddLocation.java
@@ -4,7 +4,6 @@ package net.vectorgaming.varenas.commands.admin;
 import java.util.Arrays;
 import net.vectorgaming.varenas.ArenaManager;
 import net.vectorgaming.varenas.commands.VCommand;
-import net.vectorgaming.varenas.framework.config.ArenaConfig;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -19,7 +18,7 @@ public class ArenaAddLocation extends VCommand
     public boolean run(CommandSender cs, String[] arguments)
     {
         String[] args = Arrays.copyOfRange(arguments, 1, arguments.length);
-
+                
         if(args.length != 2)
         {
             cs.sendMessage(ChatColor.RED+getUsage());
