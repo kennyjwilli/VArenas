3d2fd448d02c0aab341a0b4ecc62238c8d709ab8
Kenny Williams <kenny@vectorgaming.net>
2013-08-16 13:02:09 -0700

    Added KitFormatException to catch when the format for a kit is incorrect in the kits config

---
+++ src/net/vectorgaming/varenas/Exceptions/KitFormatException.java
@@ -0,0 +1,14 @@
+
+package net.vectorgaming.varenas.Exceptions;
+
+/**
+ *
+ * @author Kenny
+ */
+public class KitFormatException extends Exception
+{
+    public KitFormatException()
+    {
+        super("Kit is not formatted properly.");
+    }
+}
