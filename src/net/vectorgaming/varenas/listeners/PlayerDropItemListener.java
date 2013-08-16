0041a1a29b8c979e670c4c5d23426e896be8f3f7
Kenny Williams <kenny@vectorgaming.net>
2013-08-16 00:33:27 -0700

    Added a drop item listener to cancel the event if dropping items is disabled

---
+++ src/net/vectorgaming/varenas/listeners/PlayerDropItemListener.java
@@ -0,0 +1,42 @@
+
+package net.vectorgaming.varenas.listeners;
+
+import net.vectorgaming.varenas.ArenaPlayerManager;
+import net.vectorgaming.varenas.framework.Arena;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+/**
+ *
+ * @author Kenny
+ */
+public class PlayerDropItemListener implements Listener
+{
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onItemDrop(PlayerDropItemEvent event)
+    {
+        Player p = event.getPlayer();
+        
+        if(!ArenaPlayerManager.isPlayerInArena(p))
+            return;
+        
+        Arena arena = ArenaPlayerManager.getArenaFromPlayer(p);
+        
+        if(!arena.getWorld().getName().equalsIgnoreCase(p.getWorld().getName()))
+            return;
+        
+        if(!arena.isRunning())
+            return;
+        
+        if(arena.getSettings().getAllowedItemDropTypes().contains("all"))
+            return;
+        if(arena.getSettings().getAllowedItemDropTypes().contains("none"))
+        {
+            event.setCancelled(true);
+            p.getInventory().remove(event.getItemDrop().getItemStack());
+        }
+    }
+}
