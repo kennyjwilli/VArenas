1e4ec9a0af0c5a19289168b54898dcb275f1aeb9
Kenny Williams <kenny@vectorgaming.net>
2013-11-08 23:33:42 -0800

    Added ArenaChannel for a channel that will be created whenever an arena is created

---
+++ src/net/vectorgaming/varenas/chat/ArenaChannel.java
@@ -0,0 +1,17 @@
+
+package net.vectorgaming.varenas.chat;
+
+import net.vectorgaming.vchat.framework.channel.Channel;
+
+/**
+ *
+ * @author Kenny
+ */
+public class ArenaChannel extends Channel
+{
+    public ArenaChannel(String name)
+    {
+        super(name, "ARENA_CHANNEL");
+    }
+
+}
